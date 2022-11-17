877b2bc4779de1f7f4f1e24c21a21426ba8d395b
Krishna Sah <shahkrishna918@gmail.com>
2022-11-17 17:49:35 -0600

    added login and sign up

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,52 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const User = require('./models/User');
-const Message = require('./models/Message');
-const userRoutes = require('./routes/userRoutes');
-const cors = require('cors');
+const User = require("./models/User");
+const Message = require("./models/Message");
+const userRoutes = require("./routes/userRoutes");
+const cors = require("cors");
 
-const channels = ['general', 'tech', 'finance', 'crypto'];
+const channels = ["general", "tech", "finance", "crypto"];
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
-app.use('/users', userRoutes);
-require('./connection');
+app.use("/users", userRoutes);
+require("./connection");
 
-const server = require('http').createServer(app);
+const server = require("http").createServer(app);
 const PORT = 5001;
-const io = require('socket.io')(server, {
-    cors: {
-        origin: 'http://localhost:3000',
-        methods: ['GET', 'POST']
-    }
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
 });
 
-app.get('/', (req, res) => {
-    res.send("Welcome to the Chat App");
+app.get("/", (req, res) => {
+  res.send("Welcome to the Chat App");
+});
+
+app.get("/rooms", (req, res) => {
+  res.json(rooms);
+});
+
+async function getLastMessagesfromRoom(room) {
+  let roomMessages = await Message.aggregate([
+    { $match: { to: room } },
+    { $group: { id: "$date", messagesByDate: { $push: "$$ROOT" } } },
+  ]);
+  return roomMessages;
+}
+
+//socket connection
+
+io.on("connection", (socket) => {
+  socket.on("join-room", async (room) => {
+    socket.join(room);
+  });
 });
 
 server.listen(PORT, () => {
-    console.log('listening to port', PORT)
+  console.log("listening to port", PORT);
 });
--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,45 +1,78 @@
 import React from "react";
-import { Nav, Navbar, Container, NavDropdown } from 'react-bootstrap';
-import { LinkContainer } from 'react-router-bootstrap';
+import { Nav, Navbar, Container, Button, NavDropdown } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import { useLogoutUserMutation } from "../services/appApi";
+import { LinkContainer } from "react-router-bootstrap";
 import logo from "../assets/logo.png";
 
 function Navigation() {
-    return (
-        <Navbar bg="light" expand="lg">
-            <Container>
-                <LinkContainer to="/">
-                    <Navbar.Brand>
-                        <img src={logo} style={{ width: 50, height: 50 }} alt=''/>
-                    </Navbar.Brand>
-                </LinkContainer>
-                <Navbar.Brand href="#home">Chat App</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ms-auto">
-                        <LinkContainer to="/login">
-                            <Nav.Link>Login</Nav.Link>
-                        </LinkContainer>
+  const user = useSelector((state) => state.user);
+  const [logoutUser] = useLogoutUserMutation();
 
-                        <LinkContainer to="/chat">
-                            <Nav.Link>Chat</Nav.Link>
-                        </LinkContainer>
+  async function handleLogout(e) {
+    e.preventDefault();
+    await logoutUser(user);
+    window.location.replace("/");
+  }
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <LinkContainer to="/">
+          <Navbar.Brand>
+            <img src={logo} style={{ width: 50, height: 50 }} alt="" />
+          </Navbar.Brand>
+        </LinkContainer>
+        <Navbar.Brand href="#home">Chat App</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            {!user && (
+              <LinkContainer to="/login">
+                <Nav.Link>Login</Nav.Link>
+              </LinkContainer>
+            )}
 
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                                Another action
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">
-                                Separated link
-                            </NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    );
+            <LinkContainer to="/chat">
+              <Nav.Link>Chat</Nav.Link>
+            </LinkContainer>
+            {user && (
+              <NavDropdown
+                title={
+                  <>
+                    <img
+                      src={user.picture}
+                      style={{
+                        width: 30,
+                        height: 30,
+                        marginRight: 10,
+                        objectFit: "cover",
+                        borderRadius: "50%",
+                      }}
+                    />
+                  </>
+                }
+                id="basic-nav-dropdown"
+              >
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item>
+                  <Button variant="danger" onClick={handleLogout}>
+                    Logout
+                  </Button>
+                </NavDropdown.Item>
+              </NavDropdown>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default Navigation;
